c10219612786c63c5ea2774c7ccc143ac5e96cbb
yyeonjju <llilyha123@gmail.com>
2023-05-17 18:32:51 +0900

    Initial Commit

---
+++ AppleCalendar/AppleCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleCalendarApp.swift
+//  AppleCalendar
+//
+//  Created by 하연주 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct AppleCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleCalendar
+//
+//  Created by 하연주 on 2023/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
